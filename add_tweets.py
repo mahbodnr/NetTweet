4c6a87e196766ea7820a725c94c4a9c243f8066d
Mahbod <mahbodnouri@gmail.com>
2022-10-10 11:37:48 +0100

    Handle courser timeout error

--- add_tweets.py
+++ add_tweets.py
@@ -5,11 +5,10 @@ from read_config import CONFIG
 
 db = Database(CONFIG["MongoDBServer"]["connect_string"])
 # get all user ids from the database
-user_ids = db.accounts_db.find({}, {"id": 1}).sort(
-    [("last_checked", 1), ("connections", -1)]
+user_ids = list(
+    db.accounts_db.find({}, {"id": 1}).sort([("last_checked", 1), ("connections", -1)])
 )
-count = db.accounts_db.count_documents({})
-for user_id in tqdm(user_ids, total=count):
+for user_id in tqdm(user_ids):
     user = get_user(user_id["id"])
     timeline = get_timeline(user, since_id=db.get_last_tweet_id(user))
     db.add_timeline(timeline)
